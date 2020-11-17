78ce7e4313012cd3e1ab7e64ac8462b975b3e28e
Ajinkya Pophale <pophale.ajinkya@mahindra.com>
2020-11-17 18:00:43 +0530

    Added heroku settings

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // connect to mmongo db
-mongoose.connect('mongodb+srv://testuser:testuser@cluster0.a7eix.mongodb.net/got?retryWrites=true&w=majority', 
-  {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/got', 
+{useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', (err) => {
@@ -28,7 +28,7 @@ app.set('view engine', 'handlebars');
 app.use('/', require('./routes/getBattles'));
 
 // connect to server
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, (err) => {
   if(err){
     console.log(err);
